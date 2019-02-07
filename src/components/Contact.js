bca67284757d4dc49a56b3d17fd09c47538d20ca
oussou-dev <oussou.dev@gmail.com>
2019-02-07 17:49:50 +0100

    Designing the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,92 @@
 import React from "react"
+import {
+	Grid,
+	Cell,
+	List,
+	ListItem,
+	ListItemContent
+} from "react-mdl"
 
 class Contact extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1> Contact Page </h1>
+			<div className="contact-body">
+				<Grid className="contact-grid">
+					<Cell col={6}>
+						<h3>John Doe</h3>
+						<img
+							alt="avatar"
+							src="https://via.placeholder.com/150x150"
+							style={{ height: "250px" }}
+						/>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing
+							elit. Est, laboriosam fugiat facere neque assumenda,
+							repellendus magni sapiente.
+						</p>
+					</Cell>
+					<Cell col={6} phone={4}>
+						<h3> Contact Me </h3>
+						<hr />
+						<div className="contact-list>">
+							<List>
+								<ListItem>
+									<ListItemContent
+										style={{ fontSize: "20px", fontFamily: "Anton" }}
+									>
+										<i
+											className="fa fa-phone-square"
+											aria-hidden="true"
+										/>{" "}
+										00 00 00 00 00
+									</ListItemContent>
+								</ListItem>
+
+								<ListItem>
+									<ListItemContent
+										style={{ fontSize: "20px", fontFamily: "Anton" }}
+									>
+										<i className="fa fa-skype" aria-hidden="true" />{" "}
+										MySkypeId
+									</ListItemContent>
+								</ListItem>
+
+								<ListItem>
+									<ListItemContent
+										style={{ fontSize: "20px", fontFamily: "Anton" }}
+									>
+										<i
+											className="fa fa-envelope"
+											aria-hidden="true"
+										/>{" "}
+										someone@gmail.com
+									</ListItemContent>
+								</ListItem>
+
+								<ListItem>
+									<ListItemContent
+										style={{ fontSize: "20px", fontFamily: "Anton" }}
+									>
+										<i
+											className="fa fa-linkedin"
+											aria-hidden="true"
+										/>{" "}
+										LinkedinId
+									</ListItemContent>
+								</ListItem>
+
+								<ListItem>
+									<ListItemContent
+										style={{ fontSize: "20px", fontFamily: "Anton" }}
+									>
+										<i className="fa fa-twitter" aria-hidden="true" />{" "}
+										TwitterId
+									</ListItemContent>
+								</ListItem>
+							</List>
+						</div>
+					</Cell>
+				</Grid>
 			</div>
 		)
 	}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -141,7 +141,6 @@ class Projects extends React.Component {
 						</CardMenu>
 					</Card>
 
-					{/*
 					<Card shadow={5} className="projects-grid-card">
 						<CardTitle
 							style={{
@@ -170,7 +169,6 @@ class Projects extends React.Component {
 							<IconButton name="share" />
 						</CardMenu>
 					</Card>
-					*/}
 				</div>
 			)
 		} else if (this.state.activeTab === 1) {
@@ -206,10 +204,37 @@ class Projects extends React.Component {
 					}
 					ripple
 				>
-					<Tab> React </Tab>
-					<Tab> React Native </Tab>
-					<Tab> Angular </Tab>
-					<Tab> Ionic </Tab>
+					<Tab>
+						<i
+							style={{ color: "rgb(97, 218, 251)" }}
+							className="fab fa-react"
+							aria-hidden="true"
+						/>{" "}
+						React
+					</Tab>
+					<Tab>
+						<i
+							style={{ color: "#00B4DB" }}
+							className="fab fa-reacteurope"
+							aria-hidden="true"
+						/>{" "}
+						React Native
+					</Tab>
+					<Tab>
+						<i
+							style={{ color: "#e20000" }}
+							className="fab fa-angular"
+							aria-hidden="true"
+						/>{" "}
+						Angular
+					</Tab>
+					<Tab>
+						<ion-icon
+							name="logo-ionic"
+							style={{ color: "#5e2fff" }}
+						/>{" "}
+						Ionic
+					</Tab>
 				</Tabs>
 
 				<Grid>
